70dcf86a7c56296650bc50b632ea011781a8d73b
Eniwder <akabudou0120@yahoo.co.jp>
2020-12-12 21:10:31 +0900

    bug fix

--- tensokulagchecker.js
+++ tensokulagchecker.js
@@ -20,7 +20,7 @@ http.createServer(async function (req, res) {
   // レスポンスの中身を出力
   const [ip, port] = req.url.match(/ipport=(.+)/)[1].split(':');
   const lags = await check(ip, port);
-  const lagMean = parseInt(lags.average() / 16.6);
+  const lagMean = ('' + (lags.average() / 16.6)).substr(0, 3).padEnd(2, 0);
   res.end('' + lagMean);
 
 }).listen(8012, '127.0.0.1');
